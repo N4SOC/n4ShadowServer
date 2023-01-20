d7bc2f9987e46fbbbf19c7b43e3d3bbb3e101975
mrrothe <martin@rothe.uk>
2023-01-20 13:53:59 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import config
+import requests
+import json
+import hmac
+import hashlib
+
+def listReports():
+    req = {"id": "test:scan_vnc", "limit": 5, "apikey": f"{config.key}"}
+    url = 'https://transform.shadowserver.org/api2/reports/list'
+    request_string = json.dumps(req)
+
+    secret_bytes = bytes(str(config.secret), 'utf-8')
+    request_bytes = bytes(request_string, 'utf-8')
+
+    hmac_generator = hmac.new(secret_bytes, request_bytes, hashlib.sha256)
+    hmac2 = hmac_generator.hexdigest()
+
+    resp = requests.post(url, json=req, headers={"HMAC2": hmac2})
+
+    return resp.json()
+
+def downloadReport(reportID):
+    req = {"id": reportID, "limit": 5, "apikey": f"{config.key}"}
+    url = 'https://transform.shadowserver.org/api2/reports/download'
+    request_string = json.dumps(req)
+
+    secret_bytes = bytes(str(config.secret), 'utf-8')
+    request_bytes = bytes(request_string, 'utf-8')
+
+    hmac_generator = hmac.new(secret_bytes, request_bytes, hashlib.sha256)
+    hmac2 = hmac_generator.hexdigest()
+
+    resp = requests.post(url, json=req, headers={"HMAC2": hmac2})
+
+    return resp.json()
+
+for report in listReports():
+    print(report['type'])
+    reportContent=downloadReport(report['id'])
+    for device in reportContent:
+        if "ip" in device:
+            print(device['ip'])
+        else:
+            print("No IP for device")
