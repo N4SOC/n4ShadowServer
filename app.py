874278734b1ac514a7004a8228df46900fd86684
Martin Rothe <martin@rothe.uk>
2023-10-17 15:30:52 +0100

    refactored multithreading

--- app.py
+++ app.py
@@ -10,20 +10,17 @@ import azloganalytics
 
 la = azloganalytics.LogAnalytics(config.azID, config.azSecret, "shadowserver")  # Create log analytics object with creds & table
 
-subnets = True
-
 
 def genHMAC(secret, request):  # Generate SHA256 HMAC from request & secret
     request_string = json.dumps(request)
     secret_bytes = bytes(str(secret), "utf-8")
     request_bytes = bytes(request_string, "utf-8")
     hmac_generator = hmac.new(secret_bytes, request_bytes, hashlib.sha256)
-    hmac2 = hmac_generator.hexdigest()
-    return hmac2
+    return hmac_generator.hexdigest()
 
 
 def listReports():  # Get list of available reports
-    req = {"id": "", "limit": 100, "apikey": f"{config.key}"}
+    req = {"id": "", "limit": 100, "date": "2023-10-16", "apikey": f"{config.key}"}
     url = "https://transform.shadowserver.org/api2/reports/list"
     resp = requests.post(url, json=req, headers={"HMAC2": genHMAC(secret=config.secret, request=req)})
     return resp.json()
@@ -33,6 +30,7 @@ def downloadReport(reportID):  # Retreive data from specified report
     req = {"id": reportID, "limit": 10000, "apikey": f"{config.key}"}
     url = "https://transform.shadowserver.org/api2/reports/download"
     resp = requests.post(url, json=req, headers={"HMAC2": genHMAC(secret=config.secret, request=req)})
+    print(f"Downloaded Report: {reportID} | {len(resp.json())}")
     return resp.json()
 
 
@@ -57,34 +55,42 @@ def getDescription(ip):
     return smallestSubnet["description"]
 
 
-initIPAM()
+def sendRecord(scandata, scanType):
+    scandata["scan"] = scanType
+    scandata["subnet_description"] = getDescription(scandata["ip"])
+    deviceJSON = []
+    deviceJSON.append(scandata)
+    la.sendtoAzure(deviceJSON)
 
 
 def getScanData(scan):
+    print("-------------")
     if report["type"] != "device_id":
+        print(f"Start Report | {report['type']} | {report['file']} | {report['id']} ")
         reportContent = downloadReport(report["id"])
-        print(f"Report: {report['type']} - Records: {len(reportContent)}")
+        print(f"Processing Report | {report['type']} | Records: {len(reportContent)}")
         if len(reportContent) == 10000:
             print("** 10k LIMIT REACHED - Results Truncated")
         for device in reportContent:
             if "ip" in device:
-                device["scan"] = report["type"]
-                device["subnet_description"] = getDescription(device["ip"])
-                deviceJSON = []
-                deviceJSON.append(device)
-                la.sendtoAzure(deviceJSON)
+                t = threading.Thread(target=sendRecord, args=(device, report["type"]))
+                threads.append(t)
+                t.start()
+        for t in threads:
+            t.join()
+    print("-------------")
 
 
 threads = []
+try:
+    initIPAM()
+except:
+    print("Cannot reach IPAM")
+    subnets = True
 
-reportList=listReports()
+
+reportList = listReports()
 print(f"Found {len(reportList)} scans")
 
 for report in reportList:
-    print(f"Report: {report['type']}")
-    t = threading.Thread(target=getScanData, args=(report,))
-    threads.append(t)
-    t.start()
-
-for t in threads:
-    t.join()
+    getScanData(report)
